7cadeb4c577eadb5fe2c2b02019a171d0da8f0e5
Jordan de Vogelaere <email@jordandevogelaere.be>
2021-11-10 13:44:42 +0100

    Add dots to gridpainter

--- lib/src/utils/painter/grid_painter.dart
+++ lib/src/utils/painter/grid_painter.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class GridPainter extends CustomPainter {
@@ -17,23 +19,24 @@ class GridPainter extends CustomPainter {
 
   final bool isAntiAlias;
   final bool matchParentSize;
+  final bool drawDots;
 
   /// Paints a grid.
   ///
   /// Useful if added as canvas background widget.
-  GridPainter({
-    this.lineWidth = 1.0,
-    this.lineColor = Colors.black,
-    this.horizontalGap = 32.0,
-    this.verticalGap = 32.0,
-    this.offset = Offset.zero,
-    this.scale = 1.0,
-    this.showHorizontal = true,
-    this.showVertical = true,
-    this.lineLength = 1e4,
-    this.isAntiAlias = true,
-    this.matchParentSize = true,
-  });
+  GridPainter(
+      {this.lineWidth = 1.0,
+      this.lineColor = Colors.black,
+      this.horizontalGap = 32.0,
+      this.verticalGap = 32.0,
+      this.offset = Offset.zero,
+      this.scale = 1.0,
+      this.showHorizontal = true,
+      this.showVertical = true,
+      this.lineLength = 1e4,
+      this.isAntiAlias = true,
+      this.matchParentSize = true,
+      this.drawDots = false});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -52,6 +55,21 @@ class GridPainter extends CustomPainter {
       ..isAntiAlias = isAntiAlias
       ..color = lineColor
       ..strokeWidth = lineWidth;
+
+    if (drawDots) {
+      var dotPainter = Paint()
+        ..color = lineColor
+        ..strokeCap = StrokeCap.round //rounded points
+        ..strokeWidth = 5;
+      var count = (lineVerticalLength / horizontalGap).round();
+      var points = <Offset>[];
+      for (int i = -count + 1; i < count; i++) {
+        points.add((Offset(-lineHorizontalLength, i * horizontalGap) +
+                offset % horizontalGap) *
+            scale);
+      }
+      canvas.drawPoints(PointMode.points, points, dotPainter);
+    }
 
     if (showHorizontal) {
       var count = (lineVerticalLength / horizontalGap).round();
